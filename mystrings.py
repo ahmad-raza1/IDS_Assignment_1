d989f00fb716eb75312549f05d8e99216d25534a
Ahmad Raza <ahmadraza_@outlook.com>
2021-04-09 01:13:35 +0500

    Completed task # 6

---
+++ mystrings.py
@@ -0,0 +1,5 @@
+str1 = "My name is Ahmed, and I'm a Computer Science student at UCP. Currently, I am in the final semester of my degree, which means I'm almost a step away from entering so-called professional life."
+
+str2 = "Speaking of my passions, yes, of course, I've had this love for the fastest-growing technology for as long as I can remember. However, there aren't enough opportunities I see for myself here in Pakistan due to the saturation taking place almost every day. Another factor that comes along my way is, people are so judgemental, and sometimes, you don't get enough appreciation, which I believe is all-important to get \"the best\" out of you."
+
+str3 = "Moreover, I love music as well, and I wish I could learn to play drums, maybe, at some point in my life. I also want to explore different parts of the world to get the knack of diversities, origins of culture, and the living standards of people and try to document all those experiences - taking pictures and videos!"
